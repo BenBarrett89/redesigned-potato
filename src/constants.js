bca627cb3387a2d17dfd3fa39e8da280a5146ffd
Ben Barrett <benjamin.barrett@engineering.digital.dwp.gov.uk>
2019-08-08 09:48:16 +0100

    Enhanced various factors

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,21 @@
 module.exports = {
+  dateFormat: 'DD MMMM YYYY',
+  fields: {
+    name: 'Name',
+    date: 'Date',
+    referenceNumber: 'Reference Number',
+    location: 'Location'
+  },
   locations: {
-    BPV: 58.30,
-    TVP: 147.65
+    BPV: {
+      order: 0,
+      name: 'Ben Park View',
+      rate: 58.30
+    },
+    TVP: {
+      order: 1,
+      name: 'Testing View Park',
+      rate: 147.65
+    }
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const parse = require('csv-parse')
 const fs = require('fs-extra')
 const handlebars = require('handlebars')
+const moment = require('moment')
 const path = require('path')
 const prompt = require('prompt')
 const puppeteer = require('puppeteer')
@@ -19,15 +20,66 @@ const getCSV = async (filename) => {
   return csv
 }
 
+const getDate = original => {
+  const date = moment(original, constants.dateFormat)
+  const text = date.isValid() ? date.format(constants.dateFormat) : `${original} (!)`
+  return { text, date, original }
+}
+
+const getTotal = locations => {
+  const total = locations.reduce((total, location) => {
+    return total + location.calculationCost
+  }, 0)
+  return parseFloat(total).toFixed(2)
+}
+
+const sortRecords = (aRecord, bRecord) => {
+  const aDate = aRecord.date.date
+  const bDate = bRecord.date.date
+  if (!aDate.isValid()) {
+    console.log(`Warning: Invalid date found for '${aRecord.name}': ${aRecord.date.original}`)
+    return 1
+  }
+  if (!bDate.isValid()) {
+    console.log(`Warning: Invalid date found for '${bRecord.name}': ${bRecord.date.original}`)
+    return -1
+  }
+  if (aDate.isBefore(bDate)) return -1
+  else if (aDate.isAfter(bDate)) return 1
+  else {
+    const aName = aRecord.name
+    const bName = bRecord.name
+    return aName.localeCompare(bName)
+  }
+}
+
+const sortLocations = (aLocation, bLocation) => {
+  return aLocation.order - bLocation.order
+}
+
+const castFunction = (value, context) => {
+  const column = context.column
+  if (typeof (column) === 'string') {
+    let castValue
+    if (column === constants.fields.name) castValue = value
+    else if (column === constants.fields.location) castValue = value
+    else if (column === constants.fields.date) castValue = getDate(value)
+    else if (column === constants.fields.referenceNumber) castValue = value
+    return castValue
+  } else {
+    return value
+  }
+}
+
 const getLocations = output =>
   output
     .reduce((locations, record) => {
       let newLocations
       const currentRecord = {
-        name: record['Name'],
-        location: record['Location'],
-        date: record['Date'],
-        reference: record['Reference Number']
+        name: record[constants.fields.name],
+        location: record[constants.fields.location],
+        date: record[constants.fields.date],
+        reference: record[constants.fields.referenceNumber]
       }
       const existingLocationIndex = locations.findIndex(record => record.location === currentRecord.location)
       if (existingLocationIndex >= 0) {
@@ -36,35 +88,42 @@ const getLocations = output =>
         }))
         newLocations = locations
       } else {
-        newLocations = locations.concat({location: currentRecord.location, records: [currentRecord]})
+        newLocations = locations.concat({ location: currentRecord.location, records: [currentRecord] })
       }
       return newLocations
     }, [])
     .map(location => {
+      const locationConstant = constants.locations[location.location]
+      const name = locationConstant.name
+      const order = locationConstant.order
       const count = location.records.length
-      const rate = constants.locations[location.location]
-      const cost = count * rate
-      return Object.assign({}, location, { count, cost, rate })
+      const calculationRate = locationConstant.rate
+      const rate = parseFloat(calculationRate).toFixed(2)
+      const calculationCost = count * calculationRate
+      const cost = parseFloat(calculationCost).toFixed(2)
+      const records = location.records.sort(sortRecords)
+      return Object.assign({}, location, { calculationCost, count, cost, name, order, rate, records })
     })
+    .sort(sortLocations)
 
 const main = async () => {
   try {
     prompt.start()
 
     const schema = {
-        properties: {
-          filename: {
-            required: true
-          },
-          report: {
-            required: true
-          }
+      properties: {
+        filename: {
+          required: true
+        },
+        report: {
+          required: true
+        }
       }
     }
 
     const parserSettings = {
       bom: true,
-      cast: true,
+      cast: castFunction,
       columns: true,
       delimiter: ','
     }
@@ -76,32 +135,33 @@ const main = async () => {
       const csv = await getCSV(filename)
 
       parse(csv, parserSettings, async (parsingError, output) => {
-        if (parsingError) throw new Exception('Parsing error')
+        if (parsingError) throw new Error('Parsing error')
 
         const locations = getLocations(output)
+        const total = getTotal(locations)
 
         const browser = await puppeteer.launch()
         const page = await browser.newPage()
-    
-        const content = await compile('index', {report, locations})
-    
+
+        const date = moment().format(constants.dateFormat)
+
+        const content = await compile('index', { date, report, locations, total })
+
         await page.setContent(content)
         await page.emulateMedia('print')
-    
+
         await page.pdf({
           path: `${report}.pdf`,
           format: 'A4',
           printBackground: true
         })
-    
+
         console.log(`File outputted: ${report}.pdf`)
-    
+
         await browser.close()
         process.exit()
       })
-
     })
-
   } catch (error) {
     console.log(error)
   }
